f4730e1ed2593b9b2b7fb9ece16e46a55ce4f31e
dexpiper <dexpiper@yandex.ru>
2022-04-14 15:23:28 +0300

    config2

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import aiohttp
 
 
 ROOTPAGE = 'https://news.ycombinator.com/'
-TIMEOUT = aiohttp.ClientTimeout(total=5)
+TIMEOUT = aiohttp.ClientTimeout(total=5)        # total in seconds
 MAX_RETRY = 3
 MAX_WORKERS = 5
-PERIOD = 60  # in seconds
-DOWNLOADS_DIR = 'downloads'
+PERIOD = 60                                     # in seconds
+DOWNLOADS_DIR = 'downloads'                     # name of dir for downloads
 number_pattern = re.compile(r'\n(\d{1,2})\.')
 name_pattern = re.compile(r'\n\d+\. (.*)')
